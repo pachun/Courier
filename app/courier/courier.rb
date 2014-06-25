900c3d81c4b676cc9f1ef8f24899c60ef6e22c5b
Nicholas Pachulski <pachun@Nicholass-MacBook-Pro.local>
2014-06-25 15:55:15 -0400

    Commented out all the migration code. It had no tests and worked/failed randomly. Starting over with TDD :).

--- app/courier/courier.rb
+++ app/courier/courier.rb
@@ -21,6 +21,25 @@ module Courier
     end
 
     def save
+      @contexts[:main].save
+    end
+
+    # def migrate(msg = "")
+    #   if @migrator.migrate_from(@persisted_schema, to:@schema)
+    #     @schema.version = @persisted_schema.version + 1
+    #     persist_schema
+    #     log_schema(msg)
+    #   end
+    #   sync_schema_with_store
+    # end
+
+    # def last_schema
+    #   puts @migrator.logs.last[:description]
+    # end
+    #
+    # def new_schema
+    #   puts CoreData::SchemaDescription.new(@schema).describe
+    # end
 
     def new_context
       context_name = "context_" + (0...32).map{ (65+rand(26)).chr }.join
@@ -33,42 +52,42 @@ module Courier
 
     def build_all
       build_schema
-      if persisted_schema_exists?
-        return if resolve_schema_differences == :asking_to_migrate
-      else
-        persist_built_schema
-      end
+      # if persisted_schema_exists?
+      #   return if resolve_schema_differences == :asking_to_migrate
+      # else
+      #   persist_built_schema
+      # end
       sync_schema_with_store
     end
 
-    def persist_built_schema
-      @schema.version = 1
-      persist_schema
-      log_schema("First generated schema.")
-    end
+    # def persist_built_schema
+    #   @schema.version = 1
+    #   persist_schema
+    #   log_schema("First generated schema.")
+    # end
 
-    def resolve_schema_differences
-      @migrator = Migrator.load(MigrationLogSaveName)
-      @persisted_schema = persisted_schema.to_schema
-      if @persisted_schema.same_as?(@schema)
-        @schema.version = @persisted_schema.version
-      else
-        @migrator.ask_to_migrate_from(@persisted_schema, to:@schema)
-        :asking_to_migrate
-      end
-    end
+    # def resolve_schema_differences
+    #   @migrator = Migrator.load(MigrationLogSaveName)
+    #   @persisted_schema = persisted_schema.to_schema
+    #   if @persisted_schema.same_as?(@schema)
+    #     @schema.version = @persisted_schema.version
+    #   else
+    #     @migrator.ask_to_migrate_from(@persisted_schema, to:@schema)
+    #     :asking_to_migrate
+    #   end
+    # end
 
-    def log_schema(message)
-      @migrator ||= Migrator.new
-      @migrator.log(@schema, message)
-      @migrator.save(MigrationLogSaveName)
-    end
+    # def log_schema(message)
+    #   @migrator ||= Migrator.new
+    #   @migrator.log(@schema, message)
+    #   @migrator.save(MigrationLogSaveName)
+    # end
 
-    def persist_schema
-      delete_persisted_schema if persisted_schema_exists?
-      schema_description = CoreData::SchemaDescription.new(@schema)
-      schema_description.save(SchemaSaveName)
-    end
+    # def persist_schema
+    #   delete_persisted_schema if persisted_schema_exists?
+    #   schema_description = CoreData::SchemaDescription.new(@schema)
+    #   schema_description.save(SchemaSaveName)
+    # end
 
     def sync_schema_with_store
       @store_coordinator = CoreData::StoreCoordinator.new(@schema)
@@ -77,17 +96,17 @@ module Courier
       @contexts[:main].store_coordinator = @store_coordinator
     end
 
-    def persisted_schema_exists?
-      NSFileManager.defaultManager.fileExistsAtPath(Packager.URL(SchemaSaveName).path)
-    end
-
-    def persisted_schema
-      CoreData::SchemaDescription.load(SchemaSaveName)
-    end
-
-    def delete_persisted_schema
-      NSFileManager.defaultManager.removeItemAtPath(Packager.URL(SchemaSaveName).path, error:nil)
-    end
+    # def persisted_schema_exists?
+    #   NSFileManager.defaultManager.fileExistsAtPath(Packager.URL(SchemaSaveName).path)
+    # end
+    #
+    # def persisted_schema
+    #   CoreData::SchemaDescription.load(SchemaSaveName)
+    # end
+    #
+    # def delete_persisted_schema
+    #   NSFileManager.defaultManager.removeItemAtPath(Packager.URL(SchemaSaveName).path, error:nil)
+    # end
 
     def build_schema
       @schema = CoreData::Schema.new
